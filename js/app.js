90266b808ca72c69db7925bf3fbd74e39e6223fa
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-02-19 20:25:02 -0500

    added hadoukan and throwing stance

--- js/app.js
+++ js/app.js
@@ -6,5 +6,18 @@ $(document).ready(function() {
     .mouseleave(function() {
     	$('.ryu-ready').hide();
     	$('.ryu-still').show();
+    })
+	.mousedown(function() {
+    	console.log('mousedown');
+    	// play hadouken sound
+    	$('.ryu-ready').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').show();
+    	//animate it to the right of the screen
+    })
+    .mouseup(function() {
+    	console.log('mouseup');
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
     });
 });
